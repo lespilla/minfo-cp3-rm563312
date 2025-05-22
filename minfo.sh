4e1b93f138f9c3e513577f98d07548f8c9995e62
lespilla <l.spilla.c@gmail.com>
2025-05-22 15:27:28 -0300

    Update minfo.sh

--- minfo.sh
+++ minfo.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
-echo "hi"
+case "$1" in
+    hostname)
+        hostname
+        ;;
+    uptime)
+        uptime -p
+        ;;
+    disk)
+        df -h /
+        ;;
+    all)
+        echo "Hostname:"
+        hostname
+        echo -e "\nUptime:"
+        uptime -p
+        echo -e "\nDisk:"
+        df -h /
+        ;;
+*)
+    echo "Uso: ./minfo.sh {hostname|uptime|disk|all}"
+    exit 1
+    ;;
+esac
